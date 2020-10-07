20a2d0c9fe454d912643f5c6b241a58e1a1e0a61
Daniel Lemire <lemire@gmail.com>
2020-10-07 17:10:44 -0400

    Saving.

--- chapitre_3/ExerciceWhileIf.java
+++ chapitre_3/ExerciceWhileIf.java
@@ -5,17 +5,16 @@
 import javax.swing.JOptionPane;
 public class ExerciceWhileIf{
    public static void main (String args[]) {
-   
-    String chaine = JOptionPane.showInputDialog("Entrez un nombre");
-    int plusGrand = Integer.parseInt (chaine);
-    for (int compteur = 1; compteur < 5; compteur=compteur+1){
-      chaine = JOptionPane.showInputDialog("Entrez un nombre entier");
-      int entier = Integer.parseInt (chaine);
-      if (entier > plusGrand) {
-        plusGrand = entier;
+      String chaine = JOptionPane.showInputDialog("Entrez un nombre");
+      int plusGrand = Integer.parseInt (chaine);
+      for (int compteur = 1; compteur < 5; compteur=compteur+1){
+        chaine = JOptionPane.showInputDialog("Entrez un nombre entier");
+        int entier = Integer.parseInt (chaine);
+        if (entier > plusGrand) {
+          plusGrand = entier;
+        }
       }
-    }
-    JOptionPane.showMessageDialog(null,"L'entier le plus grand est " + plusGrand);
-    System.exit(0);
+      JOptionPane.showMessageDialog(null,"L'entier le plus grand est " + plusGrand);
+      System.exit(0);
     }
 }
